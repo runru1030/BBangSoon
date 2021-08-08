43b9a148666c754cf07cbc5ec76c48a3eb16da52
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-08 23:34:05 +0900

    Feat & Fix : change Loc& map paging / change to naver-map

--- src/component/Map.tsx
+++ src/component/Map.tsx
@@ -1,54 +1,95 @@
-import axios from 'axios';
+
 import * as React from 'react';
-type marker = {
-  title: string,
-  lat: number,
-  lng: number
-}
+
 type locationProps = {
-  loc: marker,
-  markerArr: marker[]
+  loc: {
+    title: string,
+    lat: number,
+    lng: number
+  },
+  setLoc: any,
+  curLoc: {
+    title: string,
+    lat: number,
+    lng: number
+  },
+  markerArr: {
+    title: string,
+    lat: number,
+    lng: number,
+  }[],
+
 }
 declare global {
   interface Window {
-    kakao: any;
+    naver: any;
   }
 }
-const Map: React.FC<locationProps> = ({ loc, markerArr }) => {
-
+const Map: React.FC<locationProps> = ({ loc, setLoc, curLoc, markerArr }) => {
+  const [mapCenter, setMapcenter] = React.useState({ title: "", lat: loc.lat, lng: loc.lng } as {
+    title: string,
+    lat: number,
+    lng: number
+  })
   React.useEffect(() => {
     map();
-  }, [loc, markerArr])
 
+  }, [loc, markerArr])
+  const onClick = () => {
+    setLoc(mapCenter)
+  }
   const map = () => {
-    let container = document.getElementById('map');
-    let options = {
-      center: new window.kakao.maps.LatLng(loc.lat, loc.lng),
-      level: 5
-    };
-    const map = new window.kakao.maps.Map(container, options);
+    const map = new window.naver.maps.Map("map", {
+      center: new window.naver.maps.LatLng(loc.lat, loc.lng),
+      zoom: 15,
+    });
+    new window.naver.maps.Marker({
+      position: new window.naver.maps.LatLng(curLoc.lat, curLoc.lng),
+      map: map,
+      icon: {
+        content: `<img src="curLoc.png" width="20px"/>`,
+      }
+    });
     markerArr.map((el) => {
 
-      new window.kakao.maps.Marker({
+      const marker = new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(el.lat, el.lng),
         map: map,
-        position: new window.kakao.maps.LatLng(el.lat, el.lng),
-        title: el.title,
+      });
+      var contentString = `
+       <div ><div id="info">${el.title}</div></div> 
+      `;
+      var infowindow = new window.naver.maps.InfoWindow({
+        content: contentString,
+        borderWidth: 0,
+        disableAnchor: true,
+        backgroundColor: 'transperant',
+      });
+      window.naver.maps.Event.addListener(marker, "click", function (event: any) {
+        if (infowindow.getMap()) {
+          infowindow.close();
+        } else {
+          infowindow.open(map, marker);
+        }
       });
     });
-    window.kakao.maps.event.addListener(map, 'center_changed', function () {
+    window.naver.maps.Event.addListener(map, 'dragend', function () {
 
+      // 지도 중심좌표를 얻어옵니다 
       var latlng = map.getCenter();
-      console.log();
+      setMapcenter(({ title: "", lat: latlng._lat, lng: latlng._lng }))
 
 
     });
   }
-  return (<div id="map" style={{
-    width: '100%',
-    height: '300px'
-  }
-  }>
+  return (<>
+    <span onClick={onClick}>현 위치에서 재검색</span>
+    <div id="map" style={{
+      width: '100%',
+      height: '300px'
+    }
+    }>
 
-  </div>);
+    </div></>);
 }
 export default Map;
--- src/routes/Surrounding.tsx
+++ src/routes/Surrounding.tsx
@@ -5,6 +5,7 @@ import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import Map from '../component/Map';
 import Nav from '../component/Nav';
+import styled from 'styled-components';
 
 type locationProps = {
     location: {
@@ -19,14 +20,27 @@ type locationProps = {
 type marker = {
     title: string,
     lat: number,
-    lng: number
+    lng: number,
+    page: number,
+    is_end_page: boolean,
 }
 const Surrounding: React.FC<locationProps> = ({ location }) => {
     const [markerArr, setMarkerArr] = React.useState<marker[]>([]);
     const [isOpen, setIsOpen] = React.useState<boolean>(false);
     const [address, setAddress] = React.useState<string>("");
     const [addressList, setAddressList] = React.useState<any[]>([]);
-    const [loc, setLoc] = React.useState<marker>({ title: "", lat: location.latitude, lng: location.longitude });
+    const [loc, setLoc] = React.useState({ title: "", lat: location.latitude, lng: location.longitude } as {
+        title: string,
+        lat: number,
+        lng: number,
+    });
+    const [curLoc, setCurLoc] = React.useState({ title: "", lat: location.latitude, lng: location.longitude } as {
+        title: string,
+        lat: number,
+        lng: number,
+    });
+    const [isEnd, setIsEnd] = React.useState<boolean>();
+    const [curpage, setCurPage] = React.useState<number>(1);
     //const [storeArr,setStoreArr]= React.useState<store[]>([]);
     const onClickChange = () => {
         setIsOpen(true);
@@ -35,6 +49,9 @@ const Surrounding: React.FC<locationProps> = ({ location }) => {
         setLoc({ title: addressList[id].place_name, lat: addressList[id].y, lng: addressList[id].x })
         setAddress(addressList[id].place_name)
         setAddressList([]);
+    }
+    const onClickNext = () => {
+        getStoreApi(curpage + 1);
     }
     const onSubmit = (event: any) => {
         event.preventDefault();
@@ -47,38 +64,51 @@ const Surrounding: React.FC<locationProps> = ({ location }) => {
             setAddressList(res.data.documents)
         })
     }
-    const getStoreApi = () => {
+    const getStoreApi = (page: number) => {
         axios.get(`https://dapi.kakao.com/v2/local/search/keyword.json?query=베이커리`, {
             headers: { Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_KEY}` },
             params: {
                 y: loc.lat,
                 x: loc.lng,
+                page: page,
                 size: 15,
                 radius: 500,
                 sort: "distance"
             }
         }).then(res => {
-            console.log(res);
-            setMarkerArr(res.data.documents.map((it: any) => ({ title: it.place_name, lng: it.x, lat: it.y })));
+            setIsEnd(res.data.meta.is_end);
+            console.log(res.data);
+            setCurPage(page + 1);
+            if (page == 1) {
+                setMarkerArr(res.data.documents.map((it: any) => ({ title: it.place_name, lng: it.x, lat: it.y })));
+            }
+            else {
+                setMarkerArr([...markerArr, ...res.data.documents.map((it: any) => ({ title: it.place_name, lng: it.x, lat: it.y }))]);
+
+            }
         })
     }
     React.useEffect(() => {
-        getStoreApi();
+        console.log(loc);
+
+        getStoreApi(1);
+
     }, [loc])
     return (<div>
-        <header><FontAwesomeIcon icon={faMapMarkerAlt} /> 내 주변 <span onClick={onClickChange}>위치 검색</span></header>
+        <Header><FontAwesomeIcon icon={faMapMarkerAlt} /> 내 주변 <span onClick={onClickChange}>위치 검색</span></Header>
         <div>
             {isOpen && <form onSubmit={onSubmit}>
                 <input type="text" value={address} onChange={(event) => setAddress(event.target.value)} />
                 <input type="submit" value="검색" />
             </form>}
 
-            {addressList.length == 0 ? <><Map loc={loc} markerArr={markerArr} /><div>
+            {addressList.length == 0 ? <><Map loc={loc} setLoc={setLoc} curLoc={curLoc} markerArr={markerArr} /><div className="scroll-list">
                 {markerArr.map((store) =>
-                    <span>{store.title}</span>
+                    <StoreList>{store.title}</StoreList>
                 )}
+                {!isEnd && <button className="more-btn" onClick={onClickNext}>더 보기</button>}
             </div></>
-                : <div>{addressList.map((it: any, idx: number) => <span id={"" + idx} onClick={(event) => onClick(event.currentTarget.id)}>{it.place_name}</span>)}
+                : <div>{addressList.map((it: any, idx: number) => <StoreList id={"" + idx} onClick={(event) => onClick(event.currentTarget.id)}>{it.place_name}</StoreList>)}
                 </div>}
 
         </div>
@@ -86,3 +116,27 @@ const Surrounding: React.FC<locationProps> = ({ location }) => {
     </div>);
 }
 export default Surrounding;
+const Header = styled.header`
+position: sticky;
+display: flex;
+align-items: center;
+padding: 20px;
+top: 0px;
+border-bottom: solid thin #d0d0d0;
+font-weight:bold;
+span{
+    font-size: x-small;
+    font-weight: normal;
+    flex: 1;
+    text-align: end;
+    
+}
+`
+const StoreList = styled.div`
+width: 100%;
+display: flex;
+align-items: center;
+padding: 20px;
+height: 30px;
+border-top: solid thin #eeeeee;
+`

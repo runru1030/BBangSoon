8cec7ab90fda8c3f543b96f54c99be587bc728f6
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-11 02:10:16 +0900

    server / Feat : mysql setting

---
+++ server/config/config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+module.exports = {
+    "development": {
+      "username": "root",
+      "password": process.env.SEQUELIZE_PASSWORD,
+      "database": "bbangsoon",
+      "host": "3.35.167.4",
+      "dialect": "mysql",
+      "operatorAliases" : false
+    }
+  };
+/* module.exports = {
+  "development": {
+    username: "root",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "dopdop_dev",
+    host: "3.35.167.4",
+    dialect: "mysql"
+  },
+  "test": {
+    username: "root",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "dopdop_test",
+    host: "3.35.167.4",
+    dialect: "mysql"
+  },
+  "production": {
+    username: "root",
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: "dopdop",
+    host: "3.35.167.4",
+    dialect: "mysql",
+    logging: false,
+  }
+}; */

31dbc3232d3b2022ee3a41b26b6b6a69d6c1c3d9
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-15 23:59:08 +0900

    server / Fix: 크롤링 수정

--- server/crawler/crawlerKakao.js
+++ server/crawler/crawlerKakao.js
@@ -1,8 +1,8 @@
 const puppeteer = require("puppeteer");
 const { Cluster } = require('puppeteer-cluster');
-const { Store, Menu, Review } = require('../models');
+const { Store, Menu, Review, StoreImg } = require('../models');
 const Op = require('Sequelize').Op
-async function crawlerKakao({ storeName, address, url }) {
+async function crawlerKakao({ id, url }) {
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT, // use one browser per worker
         maxConcurrency: 4, // cluster with four workers
@@ -10,12 +10,6 @@ async function crawlerKakao({ storeName, address, url }) {
 
     const storeCrawl = async () => {
         try {
-            const store = {
-                storeName: storeName,
-                address: address,
-                fullAddress: "",
-                telephone: ""
-            }
             const menu = {
                 tit: "",
                 price: ""
@@ -34,60 +28,37 @@ async function crawlerKakao({ storeName, address, url }) {
             });
             await page.goto(url);
             await page.waitFor(1000);
-            const sel = await page.$(".txt_address");
-            store.fullAddress = await page.evaluate((el) => el.textContent, sel);
-            const sel2 = await page.$(".txt_contact");
-            if (sel2) {
-                store.telephone = await page.evaluate((el) => el.textContent, sel2);
-            }
-            store.fullAddress = store.fullAddress.replace(/\n/g, ' ').replace(/  /g, '');
-            const [storeData, created] = await Store.findOrCreate({
-                where: {
-                    storeName: store.storeName,
-                    address: store.address,
-                    fullAddress: store.fullAddress,
-                    telephone: store.telephone
-                },
-                defaults: {
-                    storeName: store.storeName,
-                    address: store.address,
-                    fullAddress: store.fullAddress,
-                    telephone: store.telephone
-                },
-                attributes: ['id'],
-            }
-
-            );
             //menu
             for (let j = 1; ; j++) {
                 const sel = await page.$(`.list_menu > li:nth-child(${j}) > div .loss_word`)
                 if (!sel) break;
                 menu.tit = await page.evaluate((el) => el.textContent, sel);
                 const sel2 = await page.$(`.list_menu > li:nth-child(${j}) >div .price_menu`)
+                if (!sel2) break;
                 menu.price = await page.evaluate((el) => el.textContent, sel2);
                 //menuArr.push({ tit: tit, price: price })
                 menu.price = menu.price.split(" ")[1]
                 await Menu.findOrCreate({
                     where: {
-                        tit: menu.tit,
-                        price: menu.price,
-                        StoreId: storeData.id
+                        [Op.and]: [
+                            { tit: menu.tit },
+                            { price: menu.price },
+                            { StoreId: id }]
                     },
                     defaults: {
-                        tit: menu.tit,
-                        price: menu.price,
-                        StoreId: storeData.id,
+                        ...menu,
+                        StoreId: id,
                     }
 
                 })
 
             }
-            await page.close();
+            await browser.close();
         } catch (err) {
             console.log(err);
         }
     }
-    const reviewCrawl = async (StoreId) => {
+    const reviewCrawl = async () => {
         try {
             const browser = await puppeteer.launch({
                 headless: false,
@@ -107,7 +78,8 @@ async function crawlerKakao({ storeName, address, url }) {
                 const review = {
                     star: 0,
                     date: "",
-                    content: ""
+                    content: "",
+                    reviewImg: ""
                 }
                 let page_num = parseInt(((j + 4) / 5))
                 let li = parseInt((j - 1) % 5 + 1)
@@ -121,55 +93,107 @@ async function crawlerKakao({ storeName, address, url }) {
                     }
                     await page.waitFor(1000);
                 }
-                console.log(li, page_num);
                 const sel = await page.$(`.list_evaluation > li:nth-child(${li}) > .star_info .num_rate`)
                 console.log(sel);
-                if (sel == null) return;
+                if (sel == null) await browser.close();
                 review.star = parseFloat(await page.evaluate((el) => el.textContent, sel));
                 const sel2 = await page.$(`.list_evaluation > li:nth-child(${li}) > .comment_info .txt_comment > span`)
                 review.content = sel2 ? await page.evaluate((el) => el.textContent, sel2) : "";
                 const sel3 = await page.$(`.list_evaluation > li:nth-child(${li}) > .comment_info .time_write`)
                 review.date = await page.evaluate((el) => el.textContent, sel3);
-                console.log(review);
+                review.reviewImg = await page.$$eval(`.list_evaluation > li:nth-child(${li}) .img_profile`, imgs => imgs.map(img => img.getAttribute('src')));
+                review.reviewImg = review.reviewImg.length == 0 ? "" : review.reviewImg[0]
+                console.log(review.reviewImg);
                 await Review.findOrCreate({
                     where: {
                         [Op.and]: [
-                            { StoreId: StoreId },
+                            { StoreId: id },
                             { star: review.star },
-                            { date: review.date },
+                            { date: new Date(review.date) },
                             { content: review.content },
                             { nickName: "익명" }]
                     },
                     defaults: {
-                        StoreId: StoreId,
+                        StoreId: id,
                         ...review,
                         nickName: "익명"
                     }
                 })
             }
+            await browser.close();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const imgCrawl = async () => {
+        try {
+            const browser = await puppeteer.launch({
+                headless: false,
+                args: ["--window-size=1920,1080", "--disable-notifications"],
+            });
+            const page = await browser.newPage();
+            await page.setUserAgent(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
+            );
+            await page.setViewport({
+                width: 1080,
+                height: 1080,
+            });
+            await page.goto(url);
+            await page.waitFor(1000);
+            const check=await page.$("#mArticle > div.cont_photo > div.photo_area > ul > li.size_l");
+            if (check) {
+                await page.click("#mArticle > div.cont_photo > div.photo_area > ul > li.size_l")
+                await page.waitFor(1000);
+                const sel2 = await page.$(`.num_photo > .num_g`)
+                const cnt = parseInt(await page.evaluate((el) => el.textContent, sel2));
+                for (let i = 1; i <= cnt && i <= 500; i++) {
+                    const img = await page.$$eval(`.img_photo`, imgs => imgs.map(img => img.getAttribute('src')));
+                    
+                    const imgurl="https://t1.kakaocdn.net/thumb"+img[0].split("local")[1];
+                    console.log(imgurl);
+                    await StoreImg.findOrCreate({
+                        where: {
+                            [Op.and]: [
+                                { StoreId: id },
+                                { imageUrl: imgurl }]
+                        },
+                        defaults: {
+                            StoreId: id,
+                            imageUrl: imgurl
+                        }
+                    })
+                    await page.click("a.link_direction.link_next ")
+                }
+            }
+
+            await browser.close();
         } catch (err) {
             console.log(err);
         }
     }
     const getStore = () => new Promise((resolve, reject) => {
         cluster.queue(async ({ page }) => {
-            storeCrawl().then(() => {
-                page.close();
-                resolve("sucess")
-            });
+            storeCrawl().then(()=>cluster.close());
+            resolve();
+        });
+    });
+    const getReview = () => new Promise((resolve, reject) => {
+        cluster.queue(async ({ page }) => {
+            reviewCrawl().then(()=>cluster.close());
+            resolve();
         });
     })
-    const getReview = (StoreId) => new Promise((resolve, reject) => {
+    const getImg = () => new Promise((resolve, reject) => {
         cluster.queue(async ({ page }) => {
-            reviewCrawl(StoreId).then(() => {
-                page.close();
-                resolve("sucess")
-            });
+            imgCrawl().then(()=>cluster.close());
+            resolve();
         });
     })
     return {
         getStore: getStore,
-        getReview: getReview
+        getReview: getReview,
+        getImg: getImg,
     };
 };
 module.exports = crawlerKakao;

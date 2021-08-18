9a0b94d2e7ade4307c89d146d3cd6d71bf236fa4
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-19 01:01:53 +0900

    Feat : count Info(review's count, avg star) Post API

--- server/routes/storeCrawl.js
+++ server/routes/storeCrawl.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const crawlerKakao = require('../crawler/crawlerKakao');
-const { Store, Menu, Review, StoreImg, sequelize } = require('../models');
+const { Store, Menu, Review, StoreImg, sequelize, Count } = require('../models');
 
 const router = express.Router();
 router.post('/', async (req, res) => {
   try {
-    let { id, storeName,address,telephone, url } = req.body;
+    let { id, storeName, address, telephone, url } = req.body;
     const [storeData, created] = await Store.findOrCreate({
       where: { id: id },
-      attributes: ['storeName','address', 'telephone', 'id'],
-      defaults:{
+      attributes: ['storeName', 'address', 'telephone', 'id'],
+      defaults: {
         id: id,
         storeName: storeName,
         address: address,
@@ -27,9 +27,9 @@ router.post('/', async (req, res) => {
       },
       {
         model: StoreImg,
-        attributes:  ['imageUrl'],
+        attributes: ['imageUrl'],
       }
-    ]
+      ]
     })
     if (created) {
       const crw = (await crawlerKakao({ id: id, url: url }));
@@ -38,10 +38,10 @@ router.post('/', async (req, res) => {
       crw.getImg();
       return res.status(200).json(null);
     }
-    else{
+    else {
       return res.status(200).json(storeData);
     }
-      
+
   } catch (err) {
     console.log(err);
     return res.status(500).json({
@@ -53,24 +53,36 @@ router.post('/', async (req, res) => {
 router.post('/list', async (req, res) => {
   try {
     const storeArr = req.body;
-    const resultArr=[]
-    storeArr.forEach(async(element, idx) => {
+    const resultArr = []
+    await storeArr.forEach(async (element, idx) => {
       await Review.findAll({
         where: { StoreId: element.id },
-        attributes: [[sequelize.fn('count', sequelize.col('star')), 'reviewCnt'],[sequelize.fn('avg', sequelize.col('star')), 'avgStar'] ],
-      
-      }).then(result => {
-       if (result != null) {
-          const reviewData=result.map(res=>res.dataValues);
-          resultArr[idx]=reviewData;
-        } 
-      });
-      if(idx==storeArr.length-1){
-        console.log(resultArr);
-        return res.status(200).json(resultArr);
+        attributes: [[sequelize.fn('count', sequelize.col('star')), 'reviewCnt'], [sequelize.fn('avg', sequelize.col('star')), 'avgStar']],
 
-      }
+      }).then(async result => {
+        const reviewData = result.map(res => res.dataValues)[0];
+        resultArr[idx] = reviewData;
+        if (reviewData.avgStar == null) {
+          await Count.findOne({
+            where: { id: element.id },
+            attributes: ['reviewCnt', 'avgStar']
+          }).then(async res => {
+            console.log(res);
+            if (!res) {
+              const crw = (await crawlerKakao({ id: element.id, url: element.url }));
+              await crw.getcnt().then(async res => { resultArr[idx] = res });
+            }
+            else {
+              resultArr[idx] = res.dataValues;
+            }
+          })
+        }
+        if (idx == storeArr.length - 1) {
+          return res.status(200).json(resultArr)
+        }
+      });
     });
+
   } catch (err) {
     console.log(err);
     return res.status(500).json({
@@ -79,5 +91,45 @@ router.post('/list', async (req, res) => {
     });
   }
 });
+router.post('/count', async (req, res) => {
+  try {
+    const store = req.body;
+    var reviewData;
+    await Review.findAll({
+      where: { StoreId: store.id },
+      attributes: [[sequelize.fn('count', sequelize.col('star')), 'reviewCnt'], [sequelize.fn('avg', sequelize.col('star')), 'avgStar']],
 
+    }).then(async result => {
+      if (result.map(res => res.dataValues)[0].avgStar == null) {
+        await Count.findOne({
+          where: { id: store.id },
+          attributes: ['reviewCnt', 'avgStar']
+        }).then(async res => {
+          console.log(res);
+          if (!res) {
+            const crw = (await crawlerKakao({ id: store.id, url: store.url }));
+            await crw.getcnt().then(async res => { 
+              reviewData=res
+            });
+          }
+          else {
+            reviewData=res.dataValues;
+          }
+        })
+
+      }
+      else{
+        reviewData=result.map(res => res.dataValues)[0];
+      }
+    })
+    console.log(reviewData);
+    return res.status(200).json(reviewData);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: err.toString(),
+    });
+  }
+});
 module.exports = router;

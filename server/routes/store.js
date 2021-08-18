a23dad4eea024f7c49b1c7eb976143eb58ee66e1
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-19 01:00:25 +0900

    Feat : review Post API & store's Img Post API

--- server/routes/store.js
+++ server/routes/store.js
@@ -1,12 +1,104 @@
 const express = require('express');
-const { Store, Menu, Review, StoreImg } = require('../models');
+const { Store, Menu, Review, StoreImg , Count, sequelize} = require('../models');
 
 const router = express.Router();
+router.post('/list', async (req, res) => {
+  try {
+    const storeArr = req.body;
+    const resultArr=[]
+    storeArr.forEach(async(element, idx) => {
+      await Review.findAll({
+        where: { StoreId: element.id },
+        attributes: [[sequelize.fn('count', sequelize.col('star')), 'reviewCnt'],[sequelize.fn('avg', sequelize.col('star')), 'avgStar'] ],
+      
+      }).then( async result => {
+        const reviewData=result.map(res=>res.dataValues)[0];
+        resultArr[idx]=reviewData;
+       if (reviewData.avgStar == null) {
+          
+          await Count.findOne({
+            where:{id:element.id},
+            attributes:['reviewCnt', 'avgStar']
+          }).then(res=>{
+            resultArr[idx]=res?res.dataValues:reviewData;
+          })
+        }
+        if(idx==storeArr.length-1){
+          console.log(resultArr);
+          return res.status(200).json(resultArr);
+        }
+      });
+      
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: err.toString(),
+    });
+  }
+});
+router.post('/review/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    const {content, star, attach, nickName }=req.body;
+    console.log({
+      StoreId:id,
+      nickName:nickName,
+      content: content,
+      star: star,
+      date: new Date(),
+    });
+    await Review.create({
+      StoreId:id,
+      nickName:nickName,
+      content: content,
+      star: star,
+      date: new Date(),
+    })
+    const storeData = await Store.findOne({
+      where: { id: id },
+      attributes: ['storeName','address', 'telephone', 'id'],
+      order: [[Review, 'date', 'DESC']],
+      include: [{
+        model: Menu,
+        attributes: ['tit', 'price'],
+      },
+      {
+        model: Review,
+        attributes: ['star', 'content', 'date', 'nickName', 'reviewImg'],
+      },
+      {
+        model: StoreImg,
+        attributes:  ['imageUrl'],
+      }
+    ]
+    })
+    const count = await Review.findAll({
+      where: { StoreId: id },
+      attributes: [[sequelize.fn('count', sequelize.col('star')), 'reviewCnt'],[sequelize.fn('avg', sequelize.col('star')), 'avgStar'] ],
+    
+    })
+    console.log({
+      ...storeData.dataValues,...count.map(res=>res.dataValues)[0]});
+    return res.status(200).json({
+      ...storeData.dataValues,...count.map(res=>res.dataValues)[0]});
+    
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: err.toString(),
+    });
+  }
+});
 router.post('/:id', async (req, res) => {
   try {
     let id = req.params.id;
     const storeData = await Store.findOne({
       where: { id: id },
+      order: [[Review, 'date', 'DESC']],
       attributes: ['storeName','address', 'telephone', 'id'],
       include: [{
         model: Menu,
@@ -21,18 +113,12 @@ router.post('/:id', async (req, res) => {
       {
         model: StoreImg,
         attributes:  ['imageUrl'],
+        limit:3,
       }
     ]
-    }).then(result => {
-      if (result==null) {
-        return res.status(200).json(null);
-      }
-      else{
-        return res.status(200).json(result.dataValues);
-      }
-    });
+    })
 
-    //return res.status(200).json(storeData);
+    return res.status(200).json(storeData.dataValues);
    
     
   } catch (err) {
@@ -43,6 +129,25 @@ router.post('/:id', async (req, res) => {
     });
   }
 });
+router.post('/image/:id', async (req, res) => {
+  try {
+    let id = req.params.id;
+    const storeImgs= await StoreImg.findAll({
+      where: { StoreId: id },
+      order: [[Review, 'date', 'DESC']],
+      attributes: ['imageUrl'],
+      order: [['createdAt', 'DESC']]
+    })
 
-
+    return res.status(200).json(storeImgs);
+   
+    
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      error: err.toString(),
+    });
+  }
+});
 module.exports = router;

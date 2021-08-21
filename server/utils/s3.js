e53d8b2b2239104b534543c3b5232f5d0968c2ea
runru1030 <70322673+runru1030@users.noreply.github.com>
2021-08-22 03:12:18 +0900

    server / Feat : aws s3 upload

---
+++ server/utils/s3.js
@@ -0,0 +1,32 @@
+/* const AWS = require("aws-sdk");
+
+AWS.config.loadFromPath(__dirname + '/../config/s3.json');
+AWS.config.region = process.env.AWS_CONFIG_REGION;
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: process.env.AWS_CONFIG_IDENTITYPOOLID
+}); */
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+require('dotenv').config();
+const path = require('path');
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_KEYID, 
+  secretAccessKey: process.env.S3_PRIVATE_KEY,
+  region: process.env.REGION, 
+});
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'bbangsoon', // 버킷 이름
+    contentType: multerS3.AUTO_CONTENT_TYPE, // 자동을 콘텐츠 타입 세팅
+    acl: "public-read", // 클라이언트에서 자유롭게 가용하기 위함
+    key: (req, file, cb) => {
+      let extension = path.extname(file.originalname)
+      cb(null, file.fieldname + '/' + Date.now().toString()+extension);
+    }
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 } // 용량 제한
+}); 
+module.exports = upload;

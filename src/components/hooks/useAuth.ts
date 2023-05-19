315485cabb2b0222b4b7e1f01172a18f8bd6db56
runru1030 <runru1030@gmail.com>
2023-05-19 18:52:21 +0900

    fix : token undefined error

--- src/components/hooks/useAuth.ts
+++ src/components/hooks/useAuth.ts
@@ -3,12 +3,11 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useAtomValue, useSetAtom } from "jotai";
 import { useResetAtom } from "jotai/utils";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useAuth = () => {
-  const jwtToken = JSON.parse(
-    window.localStorage.getItem("token") || "null"
-  )?.access_token;
+  const [jwtToken, setJwtToken] = useState(null);
+
   const setUserAtom = useSetAtom(userInfoAtoms.userAtom);
   const user = useAtomValue(userInfoAtoms.userAtom);
   const resetUserAtom = useResetAtom(userInfoAtoms.userAtom);
@@ -32,10 +31,16 @@ const useAuth = () => {
   });
 
   useEffect(() => {
-    if (jwtToken) {
+    const token = JSON.parse(
+      window?.localStorage.getItem("token") || "null"
+    )?.access_token;
+
+    if (token) {
+      setJwtToken(token);
       axios.defaults.headers.common["Authorization"] = `${jwtToken}`;
     }
-  }, [jwtToken]);
+  }, []);
+
   useEffect(() => {
     console.log(user);
   }, [user]);

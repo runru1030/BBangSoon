b1025af4a78ecb76b096ec79ef4905a488818704
runru1030 <runru1030@gmail.com>
2023-06-26 16:25:00 +0900

    chore : add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+    worker: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@next/next/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "next",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "jsx-a11y",
+    "unused-imports",
+    "react-hooks",
+  ],
+  rules: {
+    "jsx-a11y/anchor-is-valid": "off",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "react/prop-types": "off",
+    "jsx-a11y/no-autofocus": "off",
+    // "@typescript-eslint/no-explicit-any": "off",
+    // "@typescript-eslint/no-non-null-assertion": "off",
+    // "@typescript-eslint/no-unused-vars": "off",
+    // "react-hooks/exhaustive-deps": "off",
+  },
+};

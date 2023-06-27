f9572c345d782715f2565be79dbac49125113cc3
runru1030 <runru1030@gmail.com>
2023-06-27 18:02:33 +0900

    chore : 기타 수정

--- src/lib/apis/AuthUsersApis.ts
+++ src/lib/apis/AuthUsersApis.ts
@@ -5,8 +5,9 @@ const getUser = async (email: string) => {
     const {
       data: { data },
     } = await axios.get(
-      `http://localhost:1337/api/auth-users?filters[email][$eq]=${email}`
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/auth-users?filters[email][$eq]=${email}`
     );
+
     return { attributes: { id: data[0].id, ...data[0].attributes } };
   } catch (error) {
     console.error(error);
@@ -28,9 +29,10 @@ const createUser = async ({
   try {
     const {
       data: { data },
-    } = await axios.post("http://localhost:1337/api/auth-users", {
+    } = await axios.post("${process.env.NEXT_PUBLIC_DOMAIN}/api/auth-users", {
       data: { ...userInfo, kakaoToken },
     });
+
     return { attributes: data.attributes };
   } catch (error) {
     console.error(error);
